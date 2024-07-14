2b63542e0b22fb80553662f1ff692e8dddca95f0
Diya Alam <sougr32@gmail.com>
2024-07-14 14:46:50 +0600

    Demo 1st feature 0.1

--- EMS.cpp
+++ EMS.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 
-name space std;
+using namespace std;
 
-struct Employee{
-     string name;
-     string department;
-     string position;
-    double salary;
-};
+// demo front page...............
+
+void showMenu() {
+    cout << "========================================" << endl;
+    cout << "       Employee Management System       " << endl;
+    cout << "========================================" << endl;
+    cout << "1. Admin Login" << endl;
+    cout << "2. Employee Login" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Please select an option: ";
+}
+
+int main() {
+    int choice;
+    while (true) {
+        showMenu();
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Admin Login selected" << endl;
+                // Add admin login functionality here
+                break;
+            case 2:
+                cout << "Employee Login selected" << endl;
+                // Add employee login functionality here
+                break;
+            case 3:
+                cout << "Exiting..." << endl;
+                return 0;
+            default:
+                cout << "Invalid option, please try again." << endl;
+        }
+    }
+
+    return 0;
+}
